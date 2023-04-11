2882ecaf18552e32b55e488ebe89b6f304f024a0
jackie-vifive <106610327+jackie-vifive@users.noreply.github.com>
2023-04-10 22:09:51 -0400

    Move db_client into pipeline

--- src/service/tao.rs
+++ src/service/tao.rs
@@ -21,49 +21,30 @@ pub struct QueryResponse {
 
 pub struct TaoServer {
     pub cache: Arc<Mutex<Vec<i32>>>,
-    pub db_host: String,
-    pub db_port: String,
-    pub db_name: String,
-    pub db_user: String,
-    pub db_pw: String,
 }
 
 impl TaoServer {
     pub fn new() -> Self {
         let cache = Arc::new(Mutex::new(vec![]));
 
-        dotenv::from_path("../../.env").ok();
-
-        let db_host = dotenv::var("DATABASE_HOST").unwrap();
-        let db_port = dotenv::var("DATABASE_PORT_NUM").unwrap();
-        let db_name = dotenv::var("DATABASE_NAME").unwrap();
-        let db_user = dotenv::var("DATABASE_USERNAME").unwrap();
-        let db_pw = dotenv::var("DATABASE_PASSWORD").unwrap();
-
-        TaoServer { cache, db_host, db_port, db_name, db_user, db_pw }
+        TaoServer { cache }
     }
 
-    pub fn pipeline(&self, query_input: String, encrypt: bool) -> HttpResponse {
+    pub async fn pipeline(&self, query_input: String, encrypt: bool) -> HttpResponse {
         println!("Received Query: {:#?}", query_input);
         let tao_queries = query::parser::parse(query_input.as_str());
         let sql_queries = tao_queries
             .iter()
             .map(|q| query::translator::translate(q.clone()))
             .collect::<Vec<query::query::SqlQuery>>();
-        
-        // self.db_client();
 
+        self.db_client().await;    
         return HttpResponse::Ok().json(&QueryResponse {
             response: sql_queries,
         });
     }
 
     pub async fn db_client(&self) -> Result<(), Error> {
-        let db_url = format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.db_user, self.db_pw, self.db_host, self.db_port, self.db_name
-        );
-
         let (client, connection) = tokio_postgres::connect("host=encrypted-tao.clyigb9dssrd.us-east-1.rds.amazonaws.com user=dbuser password=dbuserdbuser dbname=postgres port=5432", NoTls).await.unwrap();
 
         tokio::spawn(async move {
@@ -74,7 +55,7 @@ impl TaoServer {
 
         let query1 = client.query("
             INSERT INTO Objects (id, key, obj_type, val)
-            VALUES (123, 12, 13, 14)
+            VALUES (1234, 12, 13, 14)
             ", &[]).await?;        
         println!("{:#?}", query1);
 
@@ -93,8 +74,8 @@ pub async fn query_handler(
     tao: Data<TaoServer>,
     query: Json<QueryRequest>,
 ) -> HttpResponse {
-    TaoServer::db_client(&tao).await;
-    return TaoServer::pipeline(&tao, query.into_inner().query, false);
+    let res =  TaoServer::pipeline(&tao, query.into_inner().query, false).await;
+    return res;
 }
 
 pub fn config(cfg: &mut ServiceConfig) {
