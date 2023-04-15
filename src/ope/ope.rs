80554c6eb2bc7aef2d1594049462f2aa565ca99b
Avery Blanchard <agb2178@columbia.edu>
2023-04-15 12:43:22 -0400

    OPE: add log-gamma function
    Signed-off-by: Avery Blanchard <agb2178@columbia.edu>

--- src/ope/ope.rs
+++ src/ope/ope.rs
@@ -6,6 +6,9 @@
  *
  *      Ref: 
  *          https://faculty.cc.gatech.edu/~aboldyre/papers/bclo.pdf 
+ *          https://people.csail.mit.edu/nickolai/papers/popa-mope-eprint.pdf 
+ *          https://arxiv.org/pdf/2009.05679.pdf
+ *
  */
 
 extern crate crypto; // https://github.com/RustCrypto/traits/tree/master/crypto
@@ -29,7 +32,7 @@ pub mod ope {
     use rand::distributions::Uniform; // https://docs.rs/rand/latest/rand/distributions/uniform/index.html
     use rand_distr::{Distribution, Hypergeometric}; // https://rust-random.github.io/rand/rand_distr/struct.Hypergeometric.html
     use generic_array::{GenericArray, arr};
-
+    use std::cmp;
 
     const DEFAULT_INPUT_RANGE_START: u64 = 0;
     const DEFAULT_INPUT_RANGE_END: u64 = u16::max_value() as u64 -1;
@@ -64,8 +67,10 @@ pub mod ope {
             let ret = 1.0 * tmp / (DEFAULT_OUTPUT_RANGE_END);
 
             return  ret;
+        }
 
     }
+
     pub struct Range {
         start: u64,
         end: u64,
@@ -98,14 +103,53 @@ pub mod ope {
 
     impl OPE {
 
+        pub fn log_gamma(x: f64) -> f64 {
+            
+            let v = vec![8.333333333333333e-02, -2.777777777777778e-03,
+             7.936507936507937e-04, -5.952380952380952e-04,
+             8.417508417508418e-04, -1.917526917526918e-03,
+             6.410256410256410e-03, -2.955065359477124e-02,
+             1.796443723688307e-01, -1.39243221690590e+00];
+
+            let mut x0 = x * 1.0;
+            let mut n = 0;
+
+            if x0.eq(1.0) || x0.eq(2.0) {
+                return 0.0;
+            }
+
+            else if x.le(7.0) {
+                n = 7 - x as i64;
+                x0 = (x * 1.0) + n;
+            }
+
+            let x2 = 1.0 / (x0 * x0);
+            let xp = 2 * f64::consts::PI;
+            let mut gl0 = v[9];
+
+            for i in -1..8.rev() {
+                gl0 *= x2;
+                gl0 += a[i];
+            }
+            let mut gl = gl0 / x0 + 0.5 * xp.log() + (x0 - 0.5) * x0.log() - x0;
+
+            if x.le(7.0) {
+                for i in 1..(n+1) {
+                    gl -= (x0 - 1.0).log();
+                    x0 -= 1;
+                }
+            }
+            return gl;
+        }
         /*
-         * TO DO
          * hypergeo_sample
          *      Sample hypergeometric distribution using coins
          *      as a source of 'randomness'
          */
         pub fn hypergeo_sample(&mut self, in_range: Range, out_range: Range, seed: u64, coins: &[u8]) -> u64 {
             
+   
+            let pnrg = PNRG { coins: coins };
             let in_size = in_range.size();
             let out_size = out_range.size();
 
@@ -119,10 +163,59 @@ pub mod ope {
 
             //let hypergeo = Hypergeometric::new(index, in_size, out_size - in_size).unwrap();
 
-            if index > 10 {
+            let mut sample = 0;
+
+            if index.gt(10) {
+
+                let d1: f64 = 1.7155277699214135;
+                let d2: f64 = 0.8989161620588988;
+
+                let min: f64 = cmp::min(in_size, (out_size - in_size));
+                let size = in_size + (out_size - in_size);
+                let max: f64 = cmp::max(in_size, (out_size - in_size));
+
+                let min_sample = cmp::min(index, size - sample);
+                let d4: f64 = min as f64 / size;
+                let d5 = 1.0 - d4;
+                let d6 = min_sample * d4 + 0.5;
+                let d7 = ((size - min) * index * d4 * d5 / (size - 1) + 0.5).sqrt();
+                let d8 = d1 * d7 + d2;
+                let d9 = ((min_sample + 1) * (min + 1) / (size + 2)).floor();
+                let d10 =
+
+
                 
+
                 
             } else {
+                
+                let d1 = in_size + (out_size - in_size) - index;
+                let d2: f64 = cmp::min(in_size, (out_size - in_size));
+
+                let Y = d2;
+                let K = index;
+
+                while Y.gt(0.0)  {
+
+                    let U = prng.draw();
+                    Y -= (U + Y / (d1 + K)).floor();
+                    K -= 1;
+
+                    if K == 0 {
+                        break;
+                    }
+
+                }
+                let Z = d2 - Y;
+
+                if in_size.gt((out_size - in_size)) {
+                
+                    sample = index - Z;
+
+                }
+                
+                sample = Z;
+
 
             }
 
