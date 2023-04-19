3d15bb318cd5472d683b6158278ff1d777388987
Chris Yoon <cjy2129@columbia.edu>
2023-04-19 12:02:03 -0400

    cli and interactive

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -3,6 +3,43 @@ use encrypted_tao::service;
 use std::env;
 use std::io::{self, Write};
 
+pub struct Config {
+    pub server_addr: String,
+    pub server_port: String,
+}
+
+impl Config {
+    pub fn new(env_path: String) -> Self {
+        dotenv::from_path(env_path).ok();
+        let server_addr = dotenv::var("SERVER_ADDR").unwrap();
+        let server_port = dotenv::var("SERVER_PORT").unwrap();
+        Config { server_addr, server_port }
+    }
+}
+
+fn print_help() {
+    println!("--------------------------------------------------------------------------");
+    println!("Supported AssocTypes: FRIEND, LIKES, AUTHORED, CHECKIN");
+    println!("Supported Association Queries:");
+    println!(
+        "    ASSOC ADD id1(int) assoc(AssocType) id2(int) time(int) data(str);"
+    );
+    println!("    ASSOC GET id(int) assoc(AssocType) idset([int]);");
+    println!(
+        "    ASSOC RGET id(int) assoc(AssocType) idset([int]) time-lo(int) time-hi(int);"
+    );
+    println!("    ASSOC COUNT id(int) assoc(AssocType);");
+    println!(
+        "    ASSOC RANGE id(int) assoc(AssocType) time-lo(int) time-hi(int) lim(int);"
+    );
+    println!("");
+    println!("Supported ObjTypes: USER, COMMENT, LOCATION, POST");
+    println!("Supported Object Queries");
+    println!("    OBJ ADD id(int) obj(ObjType) data(str);");
+    println!("    OBJ GET id(int);");
+    println!("--------------------------------------------------------------------------");
+}
+
 async fn execute_tao_query(
     host: String,
     port: String,
@@ -26,58 +63,25 @@ async fn execute_tao_query(
     return res;
 }
 
-fn print_preamble(host: String, port: String) {
-    println!("==========================================================================");
-    println!("TAO COMMAND LINE INTERFACE");
-    println!("==========================================================================");
-    println!("Supported AssocTypes: FRIEND, LIKES, AUTHORED, CHECKIN");
-    println!("Supported Association Queries:");
-    println!(
-        "    ASSOC ADD id1(int) assoc(AssocType) id2(int) time(int) data(str);"
-    );
-    println!("    ASSOC GET id(int) assoc(AssocType) idset([int]);");
-    println!(
-        "    ASSOC RGET id(int) assoc(AssocType) idset([int]) lo(int) hi(int);"
-    );
-    println!("    ASSOC COUNT id(int) assoc(AssocType);");
-    println!(
-        "    ASSOC TRANGE id(int) assoc(AssocType) lo(int) hi(int) lim(int);"
-    );
-    println!("");
-    println!("Supported ObjTypes: USER, COMMENT, LOCATION, POST");
-    println!("Supported Object Queries");
-    println!("    OBJ ADD id(int) obj(ObjType) data(str);");
-    println!("    OBJ GET id(int);");
-    println!("==========================================================================");
-    println!("");
-    println!("Connecting to... host: {:#?} port: {:#?}", host, port);
-    println!("");
-}
-
 #[actix_rt::main]
 async fn main() {
+    let mut env_path = env!("CARGO_MANIFEST_DIR").to_string();
+    env_path.push_str("/.env");
+    let config = Config::new(env_path);
     let args: Vec<String> = env::args().collect();
 
-    let host = &args[1];
-    let port = &args[2];
-
-    print_preamble(host.to_string(), port.to_string());
-    loop {
-        let mut query = String::new();
-        print!("tao > ");
-        let _ = io::stdout().flush();
-        io::stdin().read_line(&mut query).expect("Failed to read line");
-        let query = query.trim_end();
-        let res = execute_tao_query(
-            host.to_string(),
-            port.to_string(),
-            query.to_string(),
-        )
-        .await;
-        println!("");
-        println!("Query: {:#?}", query);
-        println!("Result:");
-        println!("{:#?}", res);
-        println!("");
+    let query = &args[1];
+    match query.as_str() {
+        "--help" => print_help(),
+        _ => {
+            let res = execute_tao_query(
+                config.server_addr,
+                config.server_port,
+                query.to_string(),
+            )
+            .await;
+            println!("\n{:#?}\n", res)
+        }
     }
+
 }
