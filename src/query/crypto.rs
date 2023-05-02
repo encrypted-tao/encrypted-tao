3fd0a8bcd2e554868ef1cfec8f3885ebbfbf4bab
Avery Blanchard <agb2178@columbia.edu>
2023-05-02 16:45:29 -0400

    Crypto: add String encryption
    Signed-off-by: Avery Blanchard <agb2178@columbia.edu>

--- src/query/crypto.rs
+++ src/query/crypto.rs
@@ -23,7 +23,16 @@ pub fn encrypt_ope(data: u64) -> u64 {
 
 pub fn encrypt_string(data: String) -> String {
 
-    return data;
+    
+    let key = retrieve_key();
+    let mut aes_cipher =  ctr(KeySize::KeySize256, &key.into_bytes(), &[b'\x00';16]); // change IV?
+
+    let data_bytes = data.into_bytes();
+
+    aes_cipher.process(&data_bytes, &mut data_bytes.clone());
+
+    let data_string: String = data_bytes.iter().map(ToString::to_string).collect();
+    return data_string;
 }
 
 pub fn encrypt_int(data: i32) -> i32 {
@@ -34,7 +43,6 @@ pub fn encrypt_int(data: i32) -> i32 {
     let data_string = data.to_string();
     let data_bytes = data_string.into_bytes();
 
-    let mut res = 0;
     aes_cipher.process(&data_bytes, &mut data_bytes.clone());
 
     return data_bytes[0].into();
@@ -59,7 +67,7 @@ pub fn encrypt_idset(data: Vec<i32>) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
 
-    use crate::query::crypto::{encrypt_int, encrypt_idset};
+    use crate::query::crypto::{encrypt_int, encrypt_idset, encrypt_string};
 
     #[test]
     fn test_encrypt_int() {
@@ -71,5 +79,13 @@ mod tests {
     fn test_encrypt_idset() {
         let encrypt = encrypt_idset(vec![78, 2, 4, 99]);
         assert_eq!(vec![55, 50, 52, 57], encrypt);
+    }
+
+    #[test]
+    fn test_encrypt_string() {
+        let encrypt = encrypt_string("testing".to_string());
+        let test = "116101115116105110103".to_string();
+        
+        assert_eq!(encrypt, test);
     }
 }
